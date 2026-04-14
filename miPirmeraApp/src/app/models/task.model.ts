0c5ad79a26191c615c0672c126fa6bba59a4adf6
ismaron20 <isjoarca@gmail.com>
2026-04-14 16:11:57 -0600

    Crar interfaz de la tarea

---
+++ miPirmeraApp/src/app/models/task.model.ts
@@ -0,0 +1,14 @@
+export type TaskStatus = 'pendiente' | 'en progreso' | 'Listo';
+export type TaskPriority = 'baja' | 'media' | 'alta' | 'urgente';
+
+export interface Task {
+    id: string;
+    titulo: string;
+    descripcion: string;
+    fechaCreacion: string;
+    fechaLimite: string;
+    estado: TaskStatus;
+    prioridad: TaskPriority;
+    categoria: string;
+    completada: boolean;
+}
